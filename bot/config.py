7ec2d5b4d4e40bf876c3d1ca17a3bc3d69919cd1
Hotmoon <aravind1555.av@gmail.com>
2024-05-04 09:16:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -41,7 +41,7 @@ class Config:
             self.DUMP_LEECH = config("DUMP_LEECH", default=True, cast=bool)
             self.DYNO = config("DYNO", default=None)
             self.ENCODER = config("ENCODER", default=None)
-            self.EXT_CAP = config("EXTENDED_CAPTIONS", default=False, cast=bool)
+            self.EXT_CAP = config("EXTENDED_CAPTIONS", default=True, cast=bool)
             self.FBANNER = config("FBANNER", default=False, cast=bool)
             self.FCHANNEL = config("FCHANNEL", default=0, cast=int)
             self.FCHANNEL_STAT = config("FCHANNEL_STAT", default=0, cast=int)
