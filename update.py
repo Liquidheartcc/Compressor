362393ecd27f30013de5832487bad37471f4c093
Hotmoon <aravind1555.av@gmail.com>
2024-05-04 21:02:39 +0530

    Update update.py

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ try:
     AUPR = config("ALWAYS_UPDATE_PY_REQ", default=False, cast=bool)
     UPSTREAM_REPO = config(
         "UPSTREAM_REPO",
-        default="https://github.com/Liquidheartcc/Tg-Encoder")
+        default="https://github.com/Liquidheartcc/Encoder")
     UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="main")
 
 except Exception:
