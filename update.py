8cab25a82983eea9116b44e7d495703493034137
Hotmoon <aravind1555.av@gmail.com>
2024-05-04 20:54:18 +0530

    Update update.py

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ try:
     AUPR = config("ALWAYS_UPDATE_PY_REQ", default=False, cast=bool)
     UPSTREAM_REPO = config(
         "UPSTREAM_REPO",
-        default="https://github.com/Nubuki-all/Tg-encoder")
+        default="https://github.com/Liquidheartcc/Tg-Encoder")
     UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="main")
 
 except Exception:
